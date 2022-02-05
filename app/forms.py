48148f513267ab8a6a91ca82d8089900866cfcfc
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-02-05 20:57:20 +0545

    Login

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
+from multiprocessing import AuthenticationError
+from tkinter import Widget
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField
 from django.contrib.auth.models import User
+from django.utils.translation import gettext, gettext_lazy as _
 
 class CustomerRegistrationForm(UserCreationForm):
     password1=forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
@@ -11,3 +14,7 @@ class CustomerRegistrationForm(UserCreationForm):
         fields=['username','email','password1','password2']
         labels={'email':'Email'}
         widgets={'username':forms.TextInput(attrs={'class':'form-control'})}
+
+class LoginForm(AuthenticationForm):
+    username=UsernameField(widget=forms.TextInput(attrs={'autofocus':True,'class':'form-control'}))
+    password=forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password','class':'form-control'}))
--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 import imp
+from re import template
 from django.conf import settings
 from django.urls import path
 from app import views
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
+
 urlpatterns = [
     path('',views.ProductView.as_view(),name="home"),
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
@@ -15,7 +19,7 @@ urlpatterns = [
     path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
-    path('login/', views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm),name='login'),
     path('checkout/', views.checkout, name='checkout'),
     path('registration/', views.CustomerRegistrationView.as_view(), name="customerregistration")
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
