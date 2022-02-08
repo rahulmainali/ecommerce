f168e2696f0445d32f3774acb4f517761ca1149c
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-02-08 19:31:46 +0545

    place order

--- app/models.py
+++ app/models.py
@@ -47,8 +47,12 @@ class Cart(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     quantity=models.PositiveIntegerField(default=1)
 
-def __str__(self):
-    return str(self.id)
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
 
 STATE_CHOICES=(
     ('Accepted','Accepted'),
--- app/views.py
+++ app/views.py
@@ -200,7 +200,19 @@ class CustomerRegistrationView(View):
         return render(request,'app/customerregistration.html',{'form':form})
 
 def checkout(request):
- return render(request, 'app/checkout.html')
+    user=request.user
+    add=Customer.objects.filter(user=user)
+    cart_items=Cart.objects.filter(user=user)
+    amount=0.0
+    shipping_amount=60.0
+    totalamount=0.0
+    cart_product=[p for p in Cart.objects.all() if p.user==request.user]
+    if cart_product:
+        for p in cart_product:
+            tempamount=(p.quantity * p.product.discounted_price)
+            amount += tempamount
+        totalamount= amount + shipping_amount
+    return render(request, 'app/checkout.html',{'add':add,'totalamount':totalamount,'cart_items':cart_items})
 
 class ProfileView(View):
     def get(self,request):
