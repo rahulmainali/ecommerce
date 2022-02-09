86c3ac24beb980ac6187681629f7fbab5ec30b02
rahulmainali <50787264+rahulmainali@users.noreply.github.com>
2022-02-09 20:24:53 +0545

    duplicate product in cart blocked

--- app/views.py
+++ app/views.py
@@ -26,7 +26,9 @@ class ProductView(View):
 class ProductDetailView(View):
     def get(self,request,pk):
         product = Product.objects.get(pk=pk)
-        return render(request,'app/productdetail.html',{'product':product})
+        item_already_in_cart=False
+        item_already_in_cart=Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
+        return render(request,'app/productdetail.html',{'product':product,'item_already_in_cart':item_already_in_cart})
 
 
 def add_to_cart(request):
